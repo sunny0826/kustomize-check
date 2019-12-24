e8bd1354e2e34ed124bf94972bb5f70e0bb40231
guoxudong <guoxudong@keking.cn>
2019-12-24 10:17:12 +0800

    fix

--- auto-update.sh
+++ auto-update.sh
@@ -3,6 +3,9 @@
 set -e
 
 ./kust-check check
+git clone https://github.com/sunny0826/kustomize-check.git
+mv updateLog kustomize-check
+cd kustomize-check
 git add .
 git commit -am "travis automated update" || exit 0
 git push  --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:master
